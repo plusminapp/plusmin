5dcdf95745aa2a19234f8320f240dcc46c10fe9b
Ruud van Vliet <rimvanvliet@gmail.com>
2024-11-24 20:01:30 +0100

    -vaste lasten icon

---
+++ pm-frontend/src/icons/VariabeleLasten.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LayingSIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      width="24"
+      height="24"
+      {...props}
+    >
+      <g transform="rotate(90, 50, 50)">
+        <path
+          d="M50 10c-13.25 0-24 10.75-24 24s10.75 24 24 24 24 10.75 24 24-10.75 24-24 24-24-10.75-24-24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="5"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default LayingSIcon;
