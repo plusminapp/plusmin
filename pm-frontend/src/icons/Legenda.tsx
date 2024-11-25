da3515e937d62b82c9bce6e7b983e9825565ca66
Ruud van Vliet <rimvanvliet@gmail.com>
2024-11-25 15:16:48 +0100

    - nieuw iconen

--- pm-frontend/src/icons/Legenda.tsx
+++ pm-frontend/src/icons/Legenda.tsx
@@ -2,25 +2,60 @@ import React from "react";
 
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import ReorderIcon from '@mui/icons-material/Reorder';
+// import ReorderIcon from '@mui/icons-material/Reorder';
 // import WavesOutlinedIcon from '@mui/icons-material/WavesOutlined';
 import SavingsOutlinedIcon from '@mui/icons-material/SavingsOutlined';
 import TaskOutlinedIcon from '@mui/icons-material/TaskOutlined';
-import { Typography } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import VariabeleLastenIcon from "./VariabeleLasten";
+import VasteLastenIcon from "./VasteLasten";
+import KasIcon from "./Kas";
 
-const ICONS = [ArrowUpwardIcon, ShoppingCartOutlinedIcon, ReorderIcon, WavesOutlinedIcon, TaskOutlinedIcon, SavingsOutlinedIcon];
-const Legenda = ["Inkomen", "Boodschappen", "Vaste lasten", "Variabele lasten", "Aflossing", "Sparen"];
+const ICONS = [ArrowUpwardIcon, ShoppingCartOutlinedIcon, VasteLastenIcon, VariabeleLastenIcon, TaskOutlinedIcon, SavingsOutlinedIcon, KasIcon];
+const Legenda = ["Inkomen", "Boodschappen", "Vaste lasten", "Variabele lasten", "Aflossing", "Sparen", "Kas"];
+const Voorbeelden = [
+  "Salaris, uitkering, AOW, ...",
+  "Voedsel, schoonmaakmiddelen, ...",
+  "Huur, hypotheek, gemeente belastingen, enrgie, water, verzekeringen, ...",
+  "Kleding, ...",
+  "Betalingen voor een betaalregeling",
+  "Storting op/opname van de spaarrekening",
+  "Contant geld, bijvoorbeel in je portemonee"];
 
 const IconList: React.FC = () => {
   return (
-    <div style={{ display: "flex", flexDirection: 'column', gap: "16px" }}>
-      {ICONS.map((Icon, index) => (
-        <Typography variant='body1' >
-          <Icon key={index} sx={{ fontSize: 20, color: 'grey' }} />
-          &nbsp;{Legenda[index]}
-        </Typography>
-      ))}
-    </div>
+
+    <TableContainer component={Paper} sx={{ maxWidth: "xl", m: 'auto', my: '10px' }}>
+      <Table sx={{ width: "100%" }} aria-label="simple table">
+        <colgroup>
+          <col width="10%" />
+          <col width="25%" />
+          <col width="65%" />
+        </colgroup>
+        <TableHead>
+          <TableRow>
+            <TableCell>&nbsp;</TableCell>
+            <TableCell>Legenda</TableCell>
+            <TableCell>Voorbeelden</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {ICONS.map((Icon, index) => (
+            <TableRow >
+              <TableCell align="left" size='small' sx={{ p: "6px" }}>
+                <Icon key={index} sx={{ fontSize: 20, color: 'grey' }} />
+              </TableCell>
+              <TableCell>
+                {Legenda[index]}
+              </TableCell>
+              <TableCell>
+                {Voorbeelden[index]}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
--- pm-frontend/src/icons/VariabeleLasten.tsx
+++ pm-frontend/src/icons/VariabeleLasten.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import { FC, SVGProps } from 'react';
 
-const VariabeleLastenIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+const VariabeleLastenIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-    viewBox="0 0 100 100"
-    width="24"
-    height="24"
-    version="1.1"
-    id="svg186">
-   <g
-      transform="rotate(90, 50, 50)"
-      id="g184">
-     <path
-        d="M 50,1.6666667 C 35.055085,7.1751409 27.271186,11.569209 26,25.666667 c -1.189942,13.196459 11.002866,21.423759 24,24 12.826271,2.542373 24,10.75 24,24 0,13.25 -6.088983,19.762712 -24,24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="5"
-        id="path182"/>
-   </g>
- </svg>
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      version="1.1"
+      id="svg1"
+      fill="grey"
+      {...props}>
+      <path
+     d="m 17,10.09 c -2.935863,-0.02379 -3.038601,3.4 -5,3.4 -1.990611,0 -2.1010421,-3.375781 -5,-3.4 -2.8470731,-0.02379 -3.0218174,3.4 -5,3.4 v 1.95 c 3.0036164,-0.04757 3.132851,-3.4 5,-3.4 1.9678895,0 1.852753,3.41987 5,3.4 3.033602,-0.01915 3.11615,-3.4 5,-3.4 1.985023,0 2.396376,3.4 5,3.4 v -1.95 c -1.973849,0 -2.08338,-3.4 -5,-3.4"
+     id="path1" />
+    </svg>
   );
 };
 
